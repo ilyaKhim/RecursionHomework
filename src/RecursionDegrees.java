24a248b364cbc606816a3a7046551437c5d1af13
IlyaKhim <ilya.himchenko@yandex.ru>
2019-10-08 21:26:54 +0300

    test

---
+++ src/RecursionDegrees.java
@@ -0,0 +1,18 @@
+public class RecursionDegrees {
+
+        public static double calculate(double number, int degree){
+            if(degree<0){
+                return roundAvoid(1/number * (calculate(number, degree+1)),Math.abs(degree));
+            }
+            if(degree == 0) return 1;
+            if(degree == 1){
+                return number;
+            }
+            return number * (calculate(number, degree-1));
+        }
+
+        public static double roundAvoid(double value, int places) {
+            double scale = Math.pow(10, places);
+            return Math.round(value * scale) / scale;
+        }
+    }
