cc8dd822cf57f757b3fdc1b68907ee1a94c24ccf
IlyaKhim <ilya.himchenko@yandex.ru>
2019-10-08 23:32:31 +0300

    test3

--- src/RecursionDegrees.java
+++ src/RecursionDegrees.java
@@ -12,7 +12,7 @@ public class RecursionDegrees {
         }
 
 
-        private static double roundAvoid(double value, int places) {
+        public  static double roundAvoid(double value, int places) {
             double scale = Math.pow(10, places);
             return Math.round(value * scale) / scale;
         }
